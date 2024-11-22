f63692542f7acc7c0ff79793e5d51d444ce5e43f
yahia-raheem <eng.yahiaraheem.93@outlook.com>
2024-11-22 17:27:23 +0200

    Auto-generated commit

--- src/app/app.routes copy.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { BahrainPage } from './pages/bahrain/bahrain.page';
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: BahrainPage
-    }
-];
--- src/app/directives/browser-only.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BrowserOnlyDirective } from './browser-only.directive';
-
-describe('BrowserOnlyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BrowserOnlyDirective();
-    expect(directive).toBeTruthy();
-  });
-});
