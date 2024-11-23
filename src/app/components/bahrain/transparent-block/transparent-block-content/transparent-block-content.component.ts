9d1057fda7ef2cc0f5685ab3d5d3afe92836a53a
yahia-raheem <eng.yahiaraheem.93@outlook.com>
2024-11-23 18:22:46 +0200

    feat(global): add animation to transparent block

--- src/app/components/bahrain/transparent-block/transparent-block-content/transparent-block-content.component.ts
+++ src/app/components/bahrain/transparent-block/transparent-block-content/transparent-block-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ObserveVisibilityDirective } from '../../../../directives/observe-visibility.directive';
 
 @Component({
   selector: 'app-transparent-block-content',
   standalone: true,
-  imports: [],
+  imports: [ObserveVisibilityDirective],
   templateUrl: './transparent-block-content.component.html',
   styleUrl: './transparent-block-content.component.scss'
 })
--- src/app/components/bahrain/transparent-block/transparent-block.component.ts
+++ src/app/components/bahrain/transparent-block/transparent-block.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { TransparentBlockContentComponent } from "./transparent-block-content/transparent-block-content.component";
+import { ObserveVisibilityDirective } from '../../../directives/observe-visibility.directive';
 
 @Component({
   selector: 'app-transparent-block',
   standalone: true,
-  imports: [TransparentBlockContentComponent],
+  imports: [TransparentBlockContentComponent, ObserveVisibilityDirective],
   templateUrl: './transparent-block.component.html',
   styleUrl: './transparent-block.component.scss',
   encapsulation: ViewEncapsulation.None
